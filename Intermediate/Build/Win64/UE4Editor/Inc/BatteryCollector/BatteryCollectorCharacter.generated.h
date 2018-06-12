7a6df95a8d2ee010271bc5c2baed4599eaef1027
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-06-12 10:41:51 -0400

    Initiale template project creation

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollectorCharacter.generated.h
@@ -0,0 +1,94 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BATTERYCOLLECTOR_BatteryCollectorCharacter_generated_h
+#error "BatteryCollectorCharacter.generated.h already included, missing '#pragma once' in BatteryCollectorCharacter.h"
+#endif
+#define BATTERYCOLLECTOR_BatteryCollectorCharacter_generated_h
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_RPC_WRAPPERS
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABatteryCollectorCharacter(); \
+	friend BATTERYCOLLECTOR_API class UClass* Z_Construct_UClass_ABatteryCollectorCharacter(); \
+public: \
+	DECLARE_CLASS(ABatteryCollectorCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BatteryCollector"), NO_API) \
+	DECLARE_SERIALIZER(ABatteryCollectorCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesABatteryCollectorCharacter(); \
+	friend BATTERYCOLLECTOR_API class UClass* Z_Construct_UClass_ABatteryCollectorCharacter(); \
+public: \
+	DECLARE_CLASS(ABatteryCollectorCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BatteryCollector"), NO_API) \
+	DECLARE_SERIALIZER(ABatteryCollectorCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABatteryCollectorCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABatteryCollectorCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABatteryCollectorCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABatteryCollectorCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABatteryCollectorCharacter(ABatteryCollectorCharacter&&); \
+	NO_API ABatteryCollectorCharacter(const ABatteryCollectorCharacter&); \
+public:
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABatteryCollectorCharacter(ABatteryCollectorCharacter&&); \
+	NO_API ABatteryCollectorCharacter(const ABatteryCollectorCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABatteryCollectorCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABatteryCollectorCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ABatteryCollectorCharacter)
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(ABatteryCollectorCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(ABatteryCollectorCharacter, FollowCamera); }
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_9_PROLOG
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_RPC_WRAPPERS \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_INCLASS \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BatteryCollector_Source_BatteryCollector_BatteryCollectorCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/BatteryCollector/BatteryCollectorGameMode.gen.cpp
@@ -0,0 +1,69 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "BatteryCollectorGameMode.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBatteryCollectorGameMode() {}
+// Cross Module References
+	BATTERYCOLLECTOR_API UClass* Z_Construct_UClass_ABatteryCollectorGameMode_NoRegister();
+	BATTERYCOLLECTOR_API UClass* Z_Construct_UClass_ABatteryCollectorGameMode();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_BatteryCollector();
+// End Cross Module References
+	void ABatteryCollectorGameMode::StaticRegisterNativesABatteryCollectorGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_ABatteryCollectorGameMode_NoRegister()
+	{
+		return ABatteryCollectorGameMode::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ABatteryCollectorGameMode()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+				(UObject* (*)())Z_Construct_UPackage__Script_BatteryCollector,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+				{ "IncludePath", "BatteryCollectorGameMode.h" },
+				{ "ModuleRelativePath", "BatteryCollectorGameMode.h" },
+				{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+			};
+#endif
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<ABatteryCollectorGameMode>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&ABatteryCollectorGameMode::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00880288u,
+				nullptr, 0,
+				nullptr, 0,
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABatteryCollectorGameMode, 2986093520);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABatteryCollectorGameMode(Z_Construct_UClass_ABatteryCollectorGameMode, &ABatteryCollectorGameMode::StaticClass, TEXT("/Script/BatteryCollector"), TEXT("ABatteryCollectorGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABatteryCollectorGameMode);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
